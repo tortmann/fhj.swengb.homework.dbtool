9d61c5f6d8614708884b4f97879387a44c448917
Andreas Seebacher <andreas.seebacher@outlook.at>
2015-12-02 17:18:04 +0100

    added classes for tables

--- src/main/scala/fhj/swengb/homework/dbtool/DbTool.scala
+++ src/main/scala/fhj/swengb/homework/dbtool/DbTool.scala
@@ -83,28 +83,6 @@ object Db {
 
 }
 
-case class Employee(firstName: String) extends Db.DbEntity[Employee] {
-
-  // this
-  def reTable(stmt: Statement): Int = 0
-
-  def toDb(c: Connection)(t: Employee): Int = 0
-
-  def fromDb(rs: ResultSet): List[Employee] = List()
-
-
-  // this
-  def dropTableSql: String = ""
-
-  // this
-  def createTableSql: String = ""
-
-
-  //this
-  def insertSql: String = ""
-
-}
-
 
 object DbTool {
 
@@ -122,8 +100,8 @@ object DbTool {
 
 
 
-case class Article(firstName: String) extends Db.DbEntity[Article] {
-  def toDb(c: Connection)(t: Article) : Int = ???
+case class Article(artnr : Int, description :  String, price : Double) extends Db.DbEntity[Article] {
+  def toDb(c: Connection)(a: Article) : Int = ???
   def fromDb(rs: ResultSet): List[Article] = ???
 
   def reTable(stmt: Statement) : Int = {
@@ -131,7 +109,36 @@ case class Article(firstName: String) extends Db.DbEntity[Article] {
     stmt.executeUpdate(createTableSql)
   }
   def dropTableSql: String = "drop table if exists article"
-  def createTableSql: String = "create table article (githubUsername string, firstName string, secondName String, groupId integer)"
-  def insertSql: String = "insert into article (githubUsername, firstName, secondName, groupId) VALUES (?, ?, ?, ?)"
+  def createTableSql: String = "create table article (artnr integer, description string, price double)"
+  def insertSql: String = "insert into article (artnr, desc, price) VALUES (?, ?, ?)"
 
+}
+
+case class OrderPosition(ordnr : Int, pos :  Int, article : Int, text: String,amount : Int, price: Double) extends Db.DbEntity[OrderPosition] {
+  def toDb(c: Connection)(op: OrderPosition) : Int = ???
+  def fromDb(rs: ResultSet): List[OrderPosition] = ???
+
+  def reTable(stmt: Statement) : Int = {
+    stmt.executeUpdate(dropTableSql)
+    stmt.executeUpdate(createTableSql)
   }
+  def dropTableSql: String = "drop table if exists orderposition"
+  def createTableSql: String = "create table orderposition (ordnr Integer, pos   Integer, article  Integer, text String,amount  Integer, price  Double)"
+  def insertSql: String = "insert into orderposition (ordnr, pos, article, text, amount, price) VALUES (?, ?, ?, ?, ?, ?)"
+
+}
+
+
+case class Order(ordnr : Int,kdnr : Int, date : String) extends Db.DbEntity[Order] {
+  def toDb(c: Connection)(o: Order) : Int = 0
+  def fromDb(rs: ResultSet): List[Order] = List()
+
+  def reTable(stmt: Statement) : Int = {
+    stmt.executeUpdate(dropTableSql)
+    stmt.executeUpdate(createTableSql)
+  }
+  def dropTableSql: String = "drop table if exists Order"
+  def createTableSql: String = "create table Order (ordnr  Integer,kdnr  Integer, date  timestring)"
+  def insertSql: String = "insert into Order (ordnr, kdnr, date) VALUES (?, ?, ?)"
+
+}
