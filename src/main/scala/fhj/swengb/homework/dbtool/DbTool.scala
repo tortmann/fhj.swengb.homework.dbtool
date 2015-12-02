32eda0f6f13ee6c4d9d82a03511b2c7cd8bcfabd
Andreas Seebacher <andreas.seebacher@outlook.at>
2015-12-02 16:26:01 +0100

    added Article case class and companion object

--- src/main/scala/fhj/swengb/homework/dbtool/DbTool.scala
+++ src/main/scala/fhj/swengb/homework/dbtool/DbTool.scala
@@ -3,6 +3,7 @@ package fhj.swengb.homework.dbtool
 import java.sql.{Connection, DriverManager, ResultSet, Statement}
 
 import fhj.swengb.Person._
+import fhj.swengb.homework.dbtool.Article
 import fhj.swengb.{Person, Students}
 
 import scala.util.Try
@@ -84,16 +85,22 @@ object Db {
 
 case class Employee(firstName: String) extends Db.DbEntity[Employee] {
 
+  // this
   def reTable(stmt: Statement): Int = 0
 
   def toDb(c: Connection)(t: Employee): Int = 0
 
   def fromDb(rs: ResultSet): List[Employee] = List()
 
+
+  // this
   def dropTableSql: String = ""
 
+  // this
   def createTableSql: String = ""
 
+
+  //this
   def insertSql: String = ""
 
 }
@@ -111,3 +118,68 @@ object DbTool {
   }
 
 }
+
+
+
+object Article extends Db.DbEntity[Article] {
+  /**
+    * Recreates the table this entity is stored in
+    *
+    * @param stmt
+    * @return
+    */
+  def reTable(stmt: Statement): Int = {
+    stmt.executeUpdate(Article.dropTableSql)
+    stmt.executeUpdate(Article.createTableSql)
+  }
+
+  /**
+    * sql code for creating the entity backing table
+    */
+  override def createTableSql: String = ???
+
+  /**
+    * Sql code necessary to execute a drop table on the backing sql table
+    *
+    * @return
+    */
+  override def dropTableSql: String = ???
+
+  /**
+    * Saves given type to the database.
+    *
+    * @param c
+    * @param t
+    * @return
+    */
+  override def toDb(c: Connection)(t: Article): Int = ???
+
+  /**
+    * Given the resultset, it fetches its rows and converts them into instances of T
+    *
+    * @param rs
+    * @return
+    */
+  override def fromDb(rs: ResultSet): List[Article] = ???
+
+  /**
+    * sql code for inserting an entity.
+    */
+  override def insertSql: String = ???
+
+
+
+  def queryAll(con: Connection): ResultSet = query(con)("select * from article")
+}
+
+
+case class Article(firstName: String) extends Db.DbEntity[Article] {
+  def toDb(c: Connection)(t: Article) : Int = ???
+  def fromDb(rs: ResultSet): List[Article] = ???
+
+  def reTable(stmt: Statement) : Int = 0
+  val dropTableSql: String = "drop table if exists article"
+  val createTableSql: String = "create table article (githubUsername string, firstName string, secondName String, groupId integer)"
+  val insertSql: String = "insert into article (githubUsername, firstName, secondName, groupId) VALUES (?, ?, ?, ?)"
+
+  }
